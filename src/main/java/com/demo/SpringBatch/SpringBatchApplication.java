4f07921c623894ca7d9f0ce68231a1874fd16ef2
avinash2196 <avinash21961@gmail.com>
2023-06-09 03:47:27 -0400

    removed additional

--- src/main/java/com/demo/SpringBatch/SpringBatchApplication.java
+++ src/main/java/com/demo/SpringBatch/SpringBatchApplication.java
@@ -2,9 +2,7 @@ package com.demo.SpringBatch;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableBatchProcessing
